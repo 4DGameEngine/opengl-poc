1b7185507b6f93f594ec859a97892ca2fa954666
Benjamin Lim <limwenshih@hotmail.com>
2015-10-27 18:42:09 -0500

    spring cleaning

--- 3d_slices/3d_slices.cpp
+++ 3d_slices/3d_slices.cpp
@@ -2,12 +2,12 @@
 #include <set>
 #include <tuple>
 #include <vector>
+#include <cassert>
 #include "shader.h"
 #include "comvec3.h"
 #include "lin_interpolation.h"
 
-#define GLEW_STATIC
-#include <GL/glew.h>
+#define GLEW_STATIC #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <SOIL2.h>
 
@@ -27,7 +27,7 @@ GLfloat deltaTime = 0.0f;	// Time between current frame and last frame
 GLfloat lastFrame = 0.0f;  	// Time of last frame
 
 //camera properties
-glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  10.0f);
+glm::vec3 cameraPos   = glm::vec3(0.0f, 0.0f,  3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
 GLfloat yaw   = -90.0f;	// Yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right (due to how Eular angles work) so we initially rotate a bit to the left.
@@ -188,41 +188,6 @@ int main(){
         12,  5,  4
     };
     
-    GLuint lines[] = {
-         0,  1,
-         1,  2,
-         2,  3,
-         3,  0,
-         3,  7,
-         7,  4,
-         4,  0,
-         4,  5,
-         5,  1,
-         5,  6,
-         6,  2,
-         7,  6,
-         8, 11,
-        11, 15,
-        15, 12,
-        12,  8,
-         8,  9,
-         9, 10,
-        10, 11,
-        12, 13,
-        13,  9,
-        10, 14,
-        14, 13,
-        14, 15,
-         0,  8,
-         1,  9,
-         2, 10,
-         3, 11,
-         4, 12,
-         5, 13,
-         6, 14,
-         7, 15
-    };
-    
     //setup window
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -230,7 +195,7 @@ int main(){
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "3D Slice", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "3D Slice OpenGL", nullptr, nullptr);
     int Width, Height;
     glfwGetWindowSize(window, &Width, &Height);
     glfwMakeContextCurrent(window);
@@ -254,83 +219,9 @@ int main(){
 
     Shader ourShader("shaders/normal.vert", "shaders/normal.frag");
 
-    /*glm::vec4 start = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
-    glm::vec4 end   = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
-    glm::vec3 test  = lin_interpolation(start, end, 0.5);
-    std::cout<<test.x<<test.y<<test.z<<std::endl;*/
-
-    //std::set<comVec3> vertices_to_draw;
-    //std::set<comVec3>::iterator it;
-    
-    /*for (int i=0; i<64; i+=2){
-        glm::vec4 startvec = glm::vec4(vertices[lines[i]*4], vertices[lines[i]*4+1], vertices[lines[i]*4+2], vertices[lines[i]*4+3]);
-        glm::vec4 endvec = glm::vec4(vertices[lines[i+1]*4], vertices[lines[i+1]*4+1], vertices[lines[i+1]*4+2], vertices[lines[i+1]*4+3]);
-        float w=0.0;
-        std::vector<comVec3> points = lin_interpolation_l2p(startvec, endvec, w);
-        vertices_to_draw.insert(points.begin(), points.end());
-    }
-
-    std::cout<<"Vertices:"<<std::endl;
-    for (it=vertices_to_draw.begin(); it!=vertices_to_draw.end(); it++){
-        std::cout<<glm::to_string((*it).content)<<std::endl;
-    }*/
-
-    /*for (int i=64; i<96; i+=3){
-        float w=0.0;
-        glm::vec4 a = glm::vec4(vertices[indices[i]*4], vertices[indices[i]*4+1], vertices[indices[i]*4+2], vertices[indices[i]*4+3]);
-        glm::vec4 b = glm::vec4(vertices[indices[i+1]*4], vertices[indices[i+1]*4+1], vertices[indices[i+1]*4+2], vertices[indices[i+1]*4+3]);
-        glm::vec4 c = glm::vec4(vertices[indices[i+2]*4], vertices[indices[i+2]*4+1], vertices[indices[i+2]*4+2], vertices[indices[i+2]*4+3]);
-        std::vector<comVec3> points = lin_interpolation_t2l(a,b,c,w);
-        std::vector<comVec3>::iterator pit;
-        //std::cout<<vertices[indices[i]*4]<<std::endl;
-        //std::cout<<a.x<<std::endl;
-        
-        std::cout<<glm::to_string(a)<<std::endl;
-        std::cout<<glm::to_string(b)<<std::endl;
-        std::cout<<glm::to_string(c)<<std::endl;
-        for(pit=points.begin(); pit!=points.end(); pit++){
-            std::cout<<glm::to_string((*pit).content)<<std::endl;
-        }
-        std::cout<<"\n";
-    }*/
-    
-    std::vector<comVec3> list_ver;
-    std::vector<comVec3>::iterator itv=list_ver.begin();
-    float w = 0.0;
-    
-    for (int i=0; i<288; i+=36){
-        std::vector<comVec3> trigs = lin_interpolation_c2t(indices, vertices, i, 12, w);
-        //std::cout<<trigs.size()<<std::endl;
-        itv=list_ver.insert(itv, trigs.begin(), trigs.end());
-    }
-
-    
-    std::cout<<"Vertices:"<<std::endl;
-    for (itv=list_ver.begin(); itv!=list_ver.end(); itv++){
-        std::cout<<glm::to_string((*itv).content)<<std::endl;
-    }
-    std::cout<<"size: "<<list_ver.size()<<std::endl;
-    
-    std::vector<GLfloat> vertices_to_draw;
-    for (itv=list_ver.begin(); itv!=list_ver.end(); itv++){
-        vertices_to_draw.push_back((*itv).content.x);
-        vertices_to_draw.push_back((*itv).content.y);
-        vertices_to_draw.push_back((*itv).content.z);
-    }
-
-
     GLuint VAO, VBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glBindVertexArray(VAO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(&vertices_to_draw[0])*vertices_to_draw.size(), &vertices_to_draw[0], GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);  
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-    glEnable(GL_DEPTH_TEST);
 
     //main loop
     while(!glfwWindowShouldClose(window)) {
@@ -345,22 +236,53 @@ int main(){
         
         ourShader.Use();
 
+        std::vector<comVec3> list_ver;
+        std::vector<comVec3>::iterator itv=list_ver.begin();
+        float w = 0.0;
+
+        for (int i=0; i<288; i+=36){
+            std::vector<comVec3> trigs = lin_interpolation_c2t(indices, vertices, i, 12, w);
+            itv=list_ver.insert(itv, trigs.begin(), trigs.end());
+        }
+
+        std::vector<GLfloat> vertices_to_draw;
+        for (itv=list_ver.begin(); itv!=list_ver.end(); itv++){
+            vertices_to_draw.push_back((*itv).content.x);
+            vertices_to_draw.push_back((*itv).content.y);
+            vertices_to_draw.push_back((*itv).content.z);
+        }
+
+        std::cout<<"Vertices:"<<std::endl;
+        for (auto it=vertices_to_draw.begin(); it!=vertices_to_draw.end(); it++) {
+            std::cout<<*it++<<std::endl;
+            std::cout<<*it++<<std::endl;
+            std::cout<<*it<<std::endl;
+        }
+        std::cout<<"size: "<<vertices_to_draw.size()<<std::endl;
+
+        glBindVertexArray(VAO);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat)*vertices_to_draw.size(), &vertices_to_draw[0], GL_STATIC_DRAW);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+        glEnableVertexAttribArray(0);  
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glEnable(GL_DEPTH_TEST);
+
         glm::mat4 model;
         glm::mat4 view;
         glm::mat4 projection;
         model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-        model = glm::rotate(model, (GLfloat)glfwGetTime() * 1.0f, glm::vec3(0.5f, 1.0f, 0.0f)); 
+        model = glm::rotate(model, glm::radians((GLfloat)glfwGetTime() * 5.0f), glm::vec3(0.5f, 1.0f, 0.0f)); 
         view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
         projection = glm::perspective(glm::radians(fov), (GLfloat)WIDTH/(GLfloat)HEIGHT, 0.1f, 100.0f);  
-        // Get their uniform location
         GLint modelLoc = glGetUniformLocation(ourShader.Program, "model");
         GLint viewLoc = glGetUniformLocation(ourShader.Program, "view");
         GLint projLoc = glGetUniformLocation(ourShader.Program, "projection");
 
-        // Pass them to the shaders
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        // Note: currently we set the projection matrix each frame, but since the projection matrix rarely changes it's often best practice to set it outside the main loop only once.
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection)); 
 
         glBindVertexArray(VAO);
@@ -369,10 +291,9 @@ int main(){
 
         glfwSwapBuffers(window);
     }
-    
+
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    /*glDeleteBuffers(1, &EBO);    */
     glfwTerminate();
     return 0;
 }
